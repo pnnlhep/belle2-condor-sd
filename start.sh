bdfa6e47774e88b5a0708cdb3fffc23bb4cbe911
Malachi Schram <malachi.schram@gmail.com>
2016-02-06 18:47:13 -0800

    more changes...

--- start.sh
+++ start.sh
@@ -13,13 +13,13 @@
 DIRAC_BASE_PATH=/opt/dirac/
 mkdir -p ${DIRAC_BASE_PATH}
 echo ${DIRAC_BASE_PATH}
-#mkdir -p ${DIRAC_BASE_PATH}/etc/grid-security
-#ln -s /etc/grid-security/certificates  ${DIRAC_BASE_PATH}/etc/grid-security/certificates
-#cp /etc/grid-security/hostcert.pem ${DIRAC_BASE_PATH}/etc/grid-security
-#cp /etc/grid-security/hostkey.pem ${DIRAC_BASE_PATH}/etc/grid-security
+mkdir -p ${DIRAC_BASE_PATH}/etc/grid-security
+ln -s /etc/grid-security/certificates  ${DIRAC_BASE_PATH}/etc/grid-security/certificates
+cp /etc/grid-security/hostcert.pem ${DIRAC_BASE_PATH}/etc/grid-security/hostcert.pem
+cp /etc/grid-security/hostkey.pem ${DIRAC_BASE_PATH}/etc/grid-security/hostkey.pem
 chown -R belle:belle ${DIRAC_BASE_PATH}
 ls -lrt /opt/dirac/etc/grid-security
-openssl x509 -in ${DIRAC_BASE_PATH}/etc/grid-security/hostkey.pem -noout -subject
+openssl x509 -in ${DIRAC_BASE_PATH}/etc/grid-security/hostcert.pem -noout -subject
 mkdir -p /srv/dirac
 cd /srv/dirac
 wget -np https://github.com/DIRACGrid/DIRAC/raw/integration/Core/scripts/install_site.sh --no-check-certificate
